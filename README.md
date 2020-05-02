fe151802578024369a843de0546c39fdde4f054a
RomanChen <romam@romamdeMacBook-Pro.local>
2020-05-03 00:41:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 計算機
+
+[Demo](https://rccj.github.io/static-store/)
+
+
+
+## 使用技術
+
+- SCSS
+
+- JavaScript
+
+- HTML
+
---
+++ js/all.js
@@ -0,0 +1,6 @@
+(function(){
+  function menuHandler(){
+    $('.mobile-bar').stop().slideToggle()
+  }
+  $('.hamburger').click(menuHandler)
+})()
